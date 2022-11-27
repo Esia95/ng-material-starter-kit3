d050d3460d2eb0ab1a6e85c1059346b284e7067a
Estera Nowak <esteranowak95@gmail.com>
2022-11-27 15:10:34 +0100

    feat: product details implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
-
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductDetailsComponentModule } from './components/product-details/product-details.component-module';
+import { ProductServiceModule } from './services/product.service-module';
 
 @NgModule({
   imports: [RouterModule.forRoot([
-  ]), ],
+    { path: 'product/:id', component: ProductDetailsComponent }
+  ]), ProductDetailsComponentModule, ProductServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
